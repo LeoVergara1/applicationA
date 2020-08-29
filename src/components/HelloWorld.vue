ddae2b89e21179aeecbb83340ebaa326554b0394
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-28 20:26:05 -0500

    coding master  creating first component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-primary" @click="tracking()">Tracking</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-primary" @click="check_rate()">Rate</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-primary" @click="check_status()">Check Status</button>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col">
+      <div class="card text-center">
+        <div class="card-header">
+          Response
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{message}}</h5>
+        </div>
+        <div class="card-footer text-muted">
+          Status: {{status}}
+        </div>
+      </div>
+      </div>
+
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col">
+        <h4>Websocket Async</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-primary">Tracking</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-primary">Rate</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-primary">Check Status</button>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -35,6 +54,84 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function () {
+    return {
+      message: "",
+      status: "",
+      rate: {
+              "delivery": "fdex",
+              "shipper": {
+                  "name": "Sender",
+                  "company": "Company",
+                  "phone_number" : "555-555-5555",
+                  "address": "Main Street",
+                  "city": "Harrison",
+                  "state": "AR",
+                  "postal_code": "72601",
+                  "country_code": "US"
+              },
+              "recipient": {
+                  "name": "Recipient",
+                  "company": "Company",
+                  "phone_number": "555-555-5555",
+                  "address": "Main Street",
+                  "city": "Franklin Park",
+                  "state": "IL",
+                  "postal_code": "60131",
+                  "country_code": "US",
+                  "residential": "false"
+              },
+              "packages": [
+                  {
+                      "weight": {"units": "LB", "value": "6"},
+                      "dimensions": {"length": "5", "width": 5, "height": 4, "units": "IN" }
+                  }
+               ],
+              "shipping_options": {
+                "packaging_type": "YOUR_PACKAGING",
+                "drop_off_type": "REGULAR_PICKUP"
+    }
+}
+    };
+  },
+  created: function(){
+    console.log("Home created")
+
+  },
+  methods: {
+    tracking(){
+      this.$http.get('http://localhost:3000/delivery_tracking/tracking/fdex/287383').then(response => {
+        console.log(response)
+        this.message = response.body;
+      }, response => {
+        console.log(response)
+        this.message = response.body;
+        this.status = response.status
+      });
+    },
+    check_rate(){
+      this.$http.post('http://localhost:3000/delivery_tracking/rate', this.rate).then(response => {
+        console.log(response)
+        this.message = response.body;
+        this.status = response.status
+      }, response => {
+        console.log(response)
+        this.message = response.body;
+        this.status = response.status
+      });
+    },
+    check_status(){
+      this.$http.get('http://localhost:3000/delivery_tracking/satatus_delivery/fdex/287383').then(response => {
+        console.log(response)
+        this.message = response.body;
+        this.status = response.status
+      }, response => {
+        console.log(response)
+        this.message = response.body;
+        this.status = response.status
+      });
+    }
   }
 }
 </script>
